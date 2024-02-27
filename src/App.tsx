7b4c675bc8b7ba8db2a892cb94d06fc50e42c911
pb0th <panhboth.jp@gmail.com>
2024-02-27 17:13:33 +0900

    feat: changed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>MY REACT APPLICATIONNNNNNN</h3>
+      <h3>MEETING ROOM BOOKING SYSTEM</h3>
     </div>
   );
 }
